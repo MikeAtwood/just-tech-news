8c08388b121fae8118f556fbc226fbef69c7d94b
Deadpool1 <atwood169@yahoo.com>
2022-07-23 19:32:35 -0500

    added password validation

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -47,6 +47,29 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+  // expect {email: 'lernantino@gmail.com', password: 'password'}
+  User.findOne({
+    where: {
+      email: req.body.email,
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(404).json({ message: 'No user found with this email address!' });
+      return;
+    }
+
+    //res.json({ user: dbUserData });
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(404).json({ message: 'Incorrect password!' });
+      return;
+    }
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+  });
+});
+
 router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
